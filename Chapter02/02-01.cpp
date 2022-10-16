5e21be540e9c71cdbeb262e413dc8c5ec9e36e46
LoadNim <115915747+LoadNim@users.noreply.github.com>
2022-10-16 18:40:48 +0900

    Delete Chapter02 directory

--- Chapter02/02-01.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    const char* name = "LoadNim";
-    const char* address = "Korea";
-    cout<<"이름 : "<<name<<endl;
-    cout<<"주소 : "<<address<<endl;
-}
--- Chapter02/02-02.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    int mile;
-    cout<<"마일 단위 입력: ";
-    cin>>mile;
-    cout<<mile<<"마일은, "<<mile*1.60934<<"킬로미터 입니다."<<endl;
-}
