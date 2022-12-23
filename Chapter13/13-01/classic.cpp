505e7a6ed81b1dc84f561ff5b762aafbc830983b
LoadNim <ssoomin99@gmail.com>
2022-12-23 16:16:18 +0900

    프로그래밍 연습 13장

---
+++ Chapter13/13-01/classic.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstring>
+#include "classic.h"
+
+Cd::Cd(char* s1, char* s2, int n, double x){
+    strcpy(performers, s1);
+    strcpy(label, s2);
+    selections = n;
+    playtime = x;
+}
+
+Cd::Cd(const Cd& d){
+    strcpy(performers, d.performers);
+    strcpy(label, d.label);
+    selections = d.selections;
+    playtime = d.playtime;
+}
+
+Cd::Cd(){
+    strcpy(performers, nullptr);
+    strcpy(label, nullptr);
+    selections = 0;
+    playtime = 0;
+}
+
+void Cd::Report() const{
+    using std::cout;
+    using std::endl;
+
+    cout<<"연주자 : "<<performers<<endl;
+    cout<<"레이블 : "<<label<<endl;
+    cout<<"수록수 : "<<selections<<endl;
+    cout<<"곡길이 : "<<playtime<<endl;
+}
+
+Cd& Cd::operator=(const Cd& d){
+    if(this == &d) return *this;
+    strcpy(performers, d.performers);
+    strcpy(label, d.label);
+    selections = d.selections;
+    playtime = d.playtime;
+    return *this;
+}
+
+Classic::Classic(char* s1, char* s2, char* s3, int n, double x) : Cd(s2, s3, n, x){
+    strcpy(representative, s1);    
+}
+
+Classic::Classic(const Cd& d, char* s1) : Cd(d){
+    strcpy(representative, s1);
+}
+
+void Classic::Report() const{
+    Cd::Report();
+    std::cout<<"대표곡 : "<<representative<<std::endl;
+}
